b940fce7365c7416672ac0394ae20c65b999677d
esdras <esdras@talkabroad.com>
2025-08-18 00:09:11 -0600

    devolviendo controlador

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,17 @@
 class GamesController < ApplicationController
   def index
+    # Lista las carpetas dentro de app/views/games (cada carpeta es un juego)
     games_path = Rails.root.join("public", "games")
-    if Dir.exist?(games_path)
-      render plain: "si existe Available games: #{@games.inspect}"
-      @games = Dir.entries(games_path).select do |entry|
-        File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
-      end
-    else
-          render plain: "no existe Available games: #{@games.inspect}"
-      @games = []
+    @games = Dir.entries(games_path).select do |entry|
+      File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
     end
+    puts "Available games: #{@games.inspect}"  # Debugging line to check available games
+  end
 
-    # render plain: "Available games: #{@games.inspect}"
+  def show
+    game_name = params[:id]
+    render template: "games/#{game_name}/index"
+  rescue ActionView::MissingTemplate
+    render plain: "Juego no encontrado", status: :not_found
   end
 end
