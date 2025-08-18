6d94e15d0d614800a19a5f51dc20daaee03969f2
esdras <esdras@talkabroad.com>
2025-08-18 00:03:30 -0600

    probando

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,12 @@ class GamesController < ApplicationController
   def index
     games_path = Rails.root.join("public", "games")
     if Dir.exist?(games_path)
+      render plain: "si existe Available games: #{@games.inspect}"
       @games = Dir.entries(games_path).select do |entry|
         File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
       end
     else
+          render plain: "no existe Available games: #{@games.inspect}"
       @games = []
     end
 
