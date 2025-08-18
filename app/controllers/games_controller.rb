6889216f30f4b840971e1f3d7be9e293ed13b94e
esdras <esdras@talkabroad.com>
2025-08-17 23:53:26 -0600

    error en controlador

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,14 @@
 class GamesController < ApplicationController
-def index
-  games_path = Rails.root.join("public", "games")
-  if Dir.exist?(games_path)
-    @games = Dir.entries(games_path).select do |entry|
-      File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
+  def index
+    games_path = Rails.root.join("public", "games")
+    if Dir.exist?(games_path)
+      @games = Dir.entries(games_path).select do |entry|
+        File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
+      end
+    else
+      @games = []
     end
-  else
-    @games = []
-  end
 
-  render plain: "Available games: #{@games.inspect}"
+    render plain: "Available games: #{@games.inspect}"
+  end
 end
