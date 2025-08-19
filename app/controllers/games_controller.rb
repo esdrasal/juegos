c42ec6288a8df995041a3ec1918820c4fe3aeef9
esdras <esdras@talkabroad.com>
2025-08-19 09:09:56 -0600

    improving code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
     # Lista las carpetas dentro de app/views/games (cada carpeta es un juego)
     games_path = Rails.root.join("public", "games")
     @games = Dir.entries(games_path).select do |entry|
-      File.directory?(File.join(games_path, entry)) && !(entry == '.' || entry == '..')
+      File.directory?(File.join(games_path, entry)) && !(entry == "." || entry == "..")
     end
     puts "Available games: #{@games.inspect}"  # Debugging line to check available games
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'games#index'   # La página de bienvenida
+  root "games#index"   # La página de bienvenida
   get "games/:id", to: "games#show", as: "game"
 end
